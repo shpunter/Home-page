9d969cf22094c618edbe506add2e3bc510c3d736
Oleksandr <shpunterman@gmail.com>
2025-05-08 08:48:43 +0200

    add meta, robots.txt, sitemap

--- build.ts
+++ build.ts
@@ -1,1 +1,4 @@
 Deno.copyFileSync("copy/main.ts", "client/dist/main.ts");
+Deno.copyFileSync("copy/robots.txt", "client/dist/robots.txt");
+Deno.copyFileSync("copy/sitemap.xml", "client/dist/sitemap.xml");
+
