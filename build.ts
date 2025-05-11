a1de56f28a468911206cde0ef71651195763a359
Oleksandr <shpunterman@gmail.com>
2025-05-11 17:57:28 +0200

    build

--- build.ts
+++ build.ts
@@ -2,7 +2,7 @@ Deno.copyFileSync("copy/main.ts", "client/dist/main.ts");
 Deno.copyFileSync("copy/robots.txt", "client/dist/robots.txt");
 Deno.copyFileSync("copy/sitemap.xml", "client/dist/sitemap.xml");
 
-Deno.copyFileSync("copy/data.json", "client/dist/server/data.json");
-Deno.copyFileSync("copy/server.ts", "client/dist/server/server.ts");
+Deno.copyFileSync("copy/data.json", "client/dist/data.json");
+Deno.copyFileSync("copy/server.ts", "client/dist/server.ts");
 
 
