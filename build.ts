f35bc023de7ca9c7878acf44152e9fbdc40c59fa
Oleksandr <shpunterman@gmail.com>
2025-05-12 22:55:47 +0200

    typo

--- build.ts
+++ build.ts
@@ -1,4 +1,5 @@
 Deno.copyFileSync("copy/main.ts", "dist/main.ts");
 Deno.copyFileSync("copy/robots.txt", "dist/robots.txt");
 Deno.copyFileSync("copy/sitemap.xml", "dist/sitemap.xml");
-Deno.copyFileSync("copy/data.ts", "dist/data.xml");
+
+Deno.copyFileSync("copy/data.ts", "dist/data.ts");
