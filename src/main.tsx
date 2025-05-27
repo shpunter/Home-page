5635ded8d19de1f9512991174c7aaff4e9b73ffc
Oleksandr <shpunterman@gmail.com>
2025-05-27 09:34:57 +0200

    add Google tag

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import ReactDOM from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { routeTree } from "/src/routeTree.tsx";
-import ReactGA from "react-ga4";
-
-ReactGA.initialize("G-R8JHD6BVQW");
 
 const queryClient = new QueryClient()
 const router = createRouter({ routeTree });
